364ce2fbbc354afd8c12db73799760cbdb3f39da
Mika <mikaelabelgas@gmail.com>
2024-05-11 13:17:15 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { StartPage } from './start/start.page';
 
 const routes: Routes = [
   {
@@ -8,8 +9,52 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'start',
     pathMatch: 'full'
+  },
+  {
+    path: 'levels',
+    loadChildren: () => import('./levels/levels.module').then( m => m.LevelsPageModule)
+  },
+  {
+    path: 'math-quiz',
+    loadChildren: () => import('./math-quiz/math-quiz.module').then( m => m.MathQuizPageModule)
+  },
+  {
+    path: 'science-quiz',
+    loadChildren: () => import('./science-quiz/science-quiz.module').then( m => m.ScienceQuizPageModule)
+  },
+  {
+    path: 'tech-quiz',
+    loadChildren: () => import('./tech-quiz/tech-quiz.module').then( m => m.TechQuizPageModule)
+  },
+  {
+    path: 'start',
+    loadChildren: () => import('./start/start.module').then( m => m.StartPageModule)
+  },
+  {
+    path: 'math-quiz2',
+    loadChildren: () => import('./math-quiz2/math-quiz2.module').then( m => m.MathQuiz2PageModule)
+  },
+  {
+    path: 'math-quiz3',
+    loadChildren: () => import('./math-quiz3/math-quiz3.module').then( m => m.MathQuiz3PageModule)
+  },
+  {
+    path: 'science-quiz2',
+    loadChildren: () => import('./science-quiz2/science-quiz2.module').then( m => m.ScienceQuiz2PageModule)
+  },
+  {
+    path: 'science-quiz3',
+    loadChildren: () => import('./science-quiz3/science-quiz3.module').then( m => m.ScienceQuiz3PageModule)
+  },
+  {
+    path: 'sci-level',
+    loadChildren: () => import('./sci-level/sci-level.module').then( m => m.SciLevelPageModule)
+  },
+  {
+    path: 'tech-level',
+    loadChildren: () => import('./tech-level/tech-level.module').then( m => m.TechLevelPageModule)
   },
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  goToLevelsPage() {
+    this.router.navigate(['levels']);
+  }
+  goToScienceLevel() {
+    this.router.navigate(['sci-level']);
+  }
+  goToTechLevel() {
+    this.router.navigate(['tech-level']);
+  }
 
 }
+
+
